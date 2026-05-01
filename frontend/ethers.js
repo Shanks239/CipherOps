e21b9adc58951b98105113391af77f244c9411c6
Jinshi <139227943+Shanks239@users.noreply.github.com>
2026-05-01 23:29:51 +0000

    add frontend

---
+++ frontend/ethers.js
@@ -0,0 +1,14 @@
+/* esm.sh - ethers@6.13.0 */
+import "/ethers@6.13.0/es2022/abi.mjs";
+import "/ethers@6.13.0/es2022/address.mjs";
+import "/ethers@6.13.0/es2022/constants.mjs";
+import "/ethers@6.13.0/es2022/contract.mjs";
+import "/ethers@6.13.0/es2022/crypto.mjs";
+import "/ethers@6.13.0/es2022/hash.mjs";
+import "/ethers@6.13.0/es2022/lib.esm/_version.mjs";
+import "/ethers@6.13.0/es2022/providers.mjs";
+import "/ethers@6.13.0/es2022/transaction.mjs";
+import "/ethers@6.13.0/es2022/utils.mjs";
+import "/ethers@6.13.0/es2022/wallet.mjs";
+import "/ethers@6.13.0/es2022/wordlists.mjs";
+export * from "/ethers@6.13.0/es2022/ethers.mjs";
